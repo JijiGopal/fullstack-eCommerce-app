bfcb70e2bcbef07dc7a0fb31918d6928de2d64e0
Jijina KK <jijigopal2000@gmail.com>
2024-03-19 21:24:16 +0530

    created necessary files and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
- 
+import "./App.css";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
  
   return (
     <>
-       
+      <div>
+        <Navbar/>
+      </div>
     </>
   )
 }
